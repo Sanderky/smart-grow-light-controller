2d7d75e6eafe2834dc9e436fd94388a8a453aa94
Sanderky <aleksanderek.adrian@gmail.com>
2026-01-19 00:39:37 +0100

    Add font

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,6 +1,10 @@
-import { createTheme, type PaletteColor, type PaletteColorOptions } from "@mui/material/styles";
+import {
+  createTheme,
+  type PaletteColor,
+  type PaletteColorOptions,
+} from "@mui/material/styles";
 
-declare module '@mui/material/styles' {
+declare module "@mui/material/styles" {
   interface Palette {
     day: PaletteColor;
     night: PaletteColor;
@@ -11,7 +15,7 @@ declare module '@mui/material/styles' {
   }
 }
 
-declare module '@mui/material/ToggleButton' {
+declare module "@mui/material/ToggleButton" {
   interface ToggleButtonPropsColorOverrides {
     day: true;
     night: true;
@@ -20,59 +24,59 @@ declare module '@mui/material/ToggleButton' {
 
 const { palette } = createTheme();
 
-
-const createColor = (mainColor: string) => palette.augmentColor({ color: { main: mainColor } });
+const createColor = (mainColor: string) =>
+  palette.augmentColor({ color: { main: mainColor } });
 
 const theme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     // primary: {
     //   main: '#A5D6A7',
     //   light: '#d7ffd9',
     //   dark: '#75a478',
     //   contrastText: 'rgba(0, 0, 0, 0.87)'
     // },
-    primary: createColor('#5D8736'),
-    day: createColor('#E2852E'),
-    night: createColor('#6594B1'),
+    primary: createColor("#5D8736"),
+    day: createColor("#E2852E"),
+    night: createColor("#6594B1"),
 
     text: {
-      primary: '#f6f7f8',
-      secondary: '#a3beb6'
+      primary: "#f6f7f8",
+      secondary: "#a3beb6",
     },
-    divider: '#a3beb6',
+    divider: "#a3beb6",
     background: {
-      paper: '#171717',
-      default: '#1f1f1f'
-    }
+      paper: "#171717",
+      default: "#1f1f1f",
+    },
   },
 
   typography: {
+    fontFamily: ["Montserrat", "Roboto", "Arial", "sans-serif"].join(","),
     body1: {
-      color: '#a3beb6'
+      color: "#a3beb6",
     },
     h1: {
-      color: '#f6f7f8',
-      fontWeight: 'bold'
+      color: "#f6f7f8",
+      fontWeight: "bold",
     },
     h3: {
-      color: '#f6f7f8',
-      fontSize: '1.2rem',
-      fontWeight: '400'
-    }
+      color: "#f6f7f8",
+      fontSize: "1.2rem",
+      fontWeight: "400",
+    },
   },
   components: {
     MuiAlert: {
       styleOverrides: {
         standardWarning: {
-          backgroundColor: 'rgba(173, 104, 0, 0.12)',
-          border: '1px solid rgba(245, 196, 0, 0.1)',
-          color: '#f6f7f8'
-
-        }
-      }
-    }
-  }
+          backgroundColor: "rgba(173, 104, 0, 0.12)",
+          border: "1px solid rgba(245, 196, 0, 0.1)",
+          color: "#f6f7f8",
+        },
+      },
+    },
+  },
 });
 
 export default theme;
